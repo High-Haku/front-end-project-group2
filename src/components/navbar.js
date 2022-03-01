8e24374d2519e549b79a18b5a1041ed97622dedf
molzania <molzania_1507@yahoo.co.id>
2022-03-01 10:53:12 +0700

    commit on 'molly'

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import Logo from './assets/logo.png';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+function Navbar() {
+    return(
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./home.css";
+
+function Home(){
+    return(
+<div>
+<h1>Home</h1>    
+</div>
+    )
+}
+
+export default Home;

ab7057300b1a6a37a7647ee6ffce528c504a8cac
JovitoAddo <97927663+JovitoAddo@users.noreply.github.com>
2022-03-01 11:59:45 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  (
-<BrowserRouter>
-<App/>
-</BrowserRouter>),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 

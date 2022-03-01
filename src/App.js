dc0bd1f7f106d8bfcb6ead93afdc20f88bbcf336
JovitoAddo <97927663+JovitoAddo@users.noreply.github.com>
2022-03-01 11:59:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import Navbar from './components/navbar';
-import Footer from './components/footer';
-import Main from './Main.js';
-import { Routes, Route} from 'react-route';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Main/>
-      <Footer/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

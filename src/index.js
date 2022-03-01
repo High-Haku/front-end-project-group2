8532dcfec085375b745462ef134877a2d2847580
molzania <molzania_1507@yahoo.co.id>
2022-03-01 11:07:38 +0700

    commit on 'molly'

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  (
+<BrowserRouter>
+<App/>
+</BrowserRouter>),
   document.getElementById('root')
 );
 

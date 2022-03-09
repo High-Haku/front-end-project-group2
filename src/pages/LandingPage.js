bc93cc724f04b39fe07508271aa155d9c03f52fe
molzania <molzania_1507@yahoo.co.id>
2022-03-09 10:15:08 +0700

    finished navbar

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -17,6 +17,7 @@ function LandingPage(){
         <div className="LandingPage">
            <div className="header">
   <Navbar className="navbar" expand="md">
+    <Container>
     <Navbar.Brand className="navbar navbar-brand"href="#home">
       <img
         src={CatLuvLogo}
@@ -25,16 +26,17 @@ function LandingPage(){
         className="d-inline-block align-top"
         alt="CatLuv logo"
       />
-    </Navbar.Brand>      
+    </Navbar.Brand>     
     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-    <Navbar.Collapse className="navbar navbar-collapse" id="basic-navbar-nav">
-    <Nav className="me-auto">
+    <Navbar.Collapse className="navbar-collapse justify-content-end" id="basic-navbar-nav">
+    <Nav className="nav">
       <Nav.Link className="nav-link" href="#pricing">Pricing</Nav.Link>
       <Nav.Link className="nav-link" href="#courses">Courses</Nav.Link>
       <Nav.Link className="nav-link" href="#service">Service</Nav.Link>
       <Nav.Link className="nav-link" href="#company"><img alt="icon cart-icon m-r-10" width="25" height="25" src={Cart}/>Cart</Nav.Link>
     </Nav>
     </Navbar.Collapse>
+    </Container>
   </Navbar>
         </div>
         <div className="main-content">

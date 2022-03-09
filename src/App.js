710754ebf675ed32665c2da8978226f97e5ac3e9
molzania <molzania_1507@yahoo.co.id>
2022-03-09 10:18:05 +0700

    fixed header on notfound

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NotFound from "./pages/NotFound"
 function App() {
   return (
     <div>
-<LandingPage/>
+<NotFound/>
     </div>
   );
 }
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,14 +3,15 @@ import CatLuvLogo from '../img/CatLuvCom.png';
 import { Container, Navbar, Nav, Image} from "react-bootstrap";
 import ImgNotFound from "../img/img-not-found.jpeg";
 import './NotFound.css';
+import Cart from '../img/cart.svg';
 
 function NotFound(){
 return(
     <div className="NotFound">
   <div className="header">
-  <Navbar expand="lg">
-      <Container>      
-    <Navbar.Brand href="#home">
+  <Navbar className="navbar" expand="md">
+    <Container>
+    <Navbar.Brand className="navbar navbar-brand"href="#home">
       <img
         src={CatLuvLogo}
         width="200"
@@ -18,14 +19,14 @@ return(
         className="d-inline-block align-top"
         alt="CatLuv logo"
       />
-    </Navbar.Brand>      
+    </Navbar.Brand>     
     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-    <Navbar.Collapse id="basic-navbar-nav justify-content-end">
-    <Nav className="me-auto justify-content-end">
+    <Navbar.Collapse className="navbar-collapse justify-content-end" id="basic-navbar-nav">
+    <Nav className="nav">
       <Nav.Link className="nav-link" href="#pricing">Pricing</Nav.Link>
       <Nav.Link className="nav-link" href="#courses">Courses</Nav.Link>
       <Nav.Link className="nav-link" href="#service">Service</Nav.Link>
-      <Nav.Link className="nav-link" href="#company">Company</Nav.Link>
+      <Nav.Link className="nav-link" href="#company"><img alt="icon cart-icon m-r-10" width="25" height="25" src={Cart}/>Cart</Nav.Link>
     </Nav>
     </Navbar.Collapse>
     </Container>
